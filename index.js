d23868d5b207fa01a212c20d765ff9aacb817b42
hibaaahhh <hibakalathil02@gmail.com>
2024-10-20 16:40:36 +0530

    weather icons

---
+++ index.js
@@ -0,0 +1,17 @@
+const jsonServer=require('json-server')
+
+const weatherIconServer=jsonServer.create()
+
+const router=jsonServer.router('db.json')
+
+const middleware=jsonServer.defaults()
+
+const PORT = 3000 || process.env.PORT
+
+weatherIconServer.use(router)
+weatherIconServer.use(middleware)
+
+weatherIconServer.listen(PORT,()=>{
+    console.log(`MP Server has started at port ${PORT} and waiting for client request`);
+    
+})
